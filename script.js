68f59fd89f9292dc23f9aa02eca70df6a4d029bc
Salma Hanifa <119091965+salmahnf@users.noreply.github.com>
2023-08-19 21:39:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-// hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
+const ham = document.querySelector('#hamburger-menu');
+const shop = document.querySelector('#shopping-cart-button');
+
+// hamburger menu
 document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
 };
+
+//klik selain hamburger menu
+document.addEventListener('click', function (e) {
+  if (!ham.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+
+  if (!shop.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
+});
